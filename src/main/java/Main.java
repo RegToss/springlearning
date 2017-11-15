6ec44ad615ec5fc467fcee9cb2685f26a2920004
xiaqing <xiaqingweb@163.com>
2017-11-15 23:39:53 +0800

    测试

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,6 +25,6 @@ public class Main {
                 new ThreadPoolExecutor.AbortPolicy());
         executorService.execute(producer);
         executorService.execute(consumer);
-        System.out.println("分支代码1");
+        System.out.println("分支代码");
     }
 }
